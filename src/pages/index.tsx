efdad0f7842584895e33ea16837ec3c56ec39f2b
Necati Coşkunçopur <necaticoskuncopur@gmail.com>
2025-10-22 22:05:24 +0300

    feat(seo): add meta tags and Open Graph/Twitter metadata to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
 
 import styled from 'styled-components';
 
 import { About, Footer, Header, Intro, Portfolio } from '@/components';
 import theme from '@/theme';
+import data from '../data.json';
 
 const { colors, device } = theme;
 const Home = () => {
@@ -19,15 +21,65 @@ const Home = () => {
   }, []);
 
   return (
-    <Wrapper>
-      <Header isScrolled={isScrolled} />
-      <Content $isScrolled={isScrolled}>
-        <Intro />
-        <About />
-        <Portfolio />
-      </Content>
-      <Footer />
-    </Wrapper>
+    <>
+      <Head>
+        <title>{data.name}</title>
+        <meta
+          name="description"
+          content={`${data.role} ${data.name} portfolio: projects, skills, and contact information.`}
+        />
+        <link
+          rel="canonical"
+          href="https://www.tolgakilic.com/"
+        />
+        <meta
+          property="og:title"
+          content={`${data.name} - Portfolio`}
+        />
+        <meta
+          property="og:description"
+          content={`${data.role} ${data.name} portfolio: projects, skills, and contact information.`}
+        />
+        <meta
+          property="og:type"
+          content="website"
+        />
+        <meta
+          property="og:url"
+          content="https://www.tolgakilic.com/"
+        />
+        <meta
+          property="og:image"
+          content={data.avatar}
+        />
+
+        <meta
+          name="twitter:card"
+          content="summary_large_image"
+        />
+        <meta
+          name="twitter:title"
+          content={`${data.name} - Portfolio`}
+        />
+        <meta
+          name="twitter:description"
+          content={`${data.role} ${data.name} portfolio: projects, skills, and contact information.`}
+        />
+        <meta
+          name="twitter:image"
+          content={data.avatar}
+        />
+      </Head>
+      <Wrapper>
+        <Header isScrolled={isScrolled} />
+        <Content $isScrolled={isScrolled}>
+          <Intro />
+          <About />
+          <Portfolio />
+        </Content>
+        <Footer />
+      </Wrapper>
+    </>
   );
 };
 
